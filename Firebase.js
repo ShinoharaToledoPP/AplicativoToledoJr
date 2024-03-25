e3b825b6a85acd4dd3b506ed14fdc0e40ab5edcf
amshinohara <88862748+amshinohara@users.noreply.github.com>
2024-03-25 14:32:18 -0300

    Add files via upload

---
+++ Firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsOkoJAOmgxEcblnmhIlcOULGB8ctgXM0",
+  authDomain: "apptoledojr.firebaseapp.com",
+  databaseURL: "https://apptoledojr-default-rtdb.firebaseio.com",
+  projectId: "apptoledojr",
+  storageBucket: "apptoledojr.appspot.com",
+  messagingSenderId: "372137804523",
+  appId: "1:372137804523:web:933453b31b12bee791b4ae"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
